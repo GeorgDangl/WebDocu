9fb3c0fa5f4fb31440c085726c1b479019998aa1
Georg <georg@georgdangl.de>
2015-12-28 15:57:06 +0100

    Initial Commit

---
+++ src/Dangl.WebDocumentation.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dangl.WebDocumentation.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dangl.WebDocumentation.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2af9048d-8b53-4de7-a2f8-6a4a7151c414")]
---
+++ src/Dangl.WebDocumentation/Models/DatabaseInitialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dangl.WebDocumentation.Models
+{
+    public static class DatabaseInitialization
+    {
+        public static void Initialize(ApplicationDbContext Context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
