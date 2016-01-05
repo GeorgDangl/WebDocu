5870ef23299b4c751a964ac957caef6db841bb4b
Georg <georg@georgdangl.de>
2016-01-05 13:37:26 +0100

    Cleanup in Repository

--- src/Dangl.WebDocumentation/Repository/ProjectWriter.cs
+++ src/Dangl.WebDocumentation/Repository/ProjectWriter.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Threading.Tasks;
 using Dangl.WebDocumentation.Models;
-using Microsoft.AspNet.Hosting;
 
 namespace Dangl.WebDocumentation.Repository
 {
@@ -13,44 +10,52 @@ namespace Dangl.WebDocumentation.Repository
     {
         public static bool CreateProjectFilesFromZip(ZipArchive projectArchive, string physicalProjectsRootDirectory, Guid projectId, ApplicationDbContext context)
         {
-            var databaseProject = context.DocumentationProjects.FirstOrDefault(Project => Project.Id == projectId);
-            if (databaseProject == null)
+            try
             {
-                throw new ArgumentException(nameof(projectId) + " is not a valid Guid for a project.");
-            }
-            var newGuid = Guid.NewGuid();
-            // Generate a Guid under which to store the upload
-            var newProjectDir = Path.Combine(physicalProjectsRootDirectory, newGuid.ToString());
-            Directory.CreateDirectory(newProjectDir);
-            foreach (var fileEntry in projectArchive.Entries)
-            {
-                var neededPath = new FileInfo(Path.Combine(newProjectDir, fileEntry.FullName)).Directory.FullName;
-                if (!Directory.Exists(neededPath))
+                var databaseProject = context.DocumentationProjects.FirstOrDefault(Project => Project.Id == projectId);
+                if (databaseProject == null)
                 {
-                    Directory.CreateDirectory(neededPath);
+                    throw new ArgumentException(nameof(projectId) + " is not a valid Guid for a project.");
                 }
-                // Copy only when it's a file and not a folder
-                if (fileEntry.Length > 0)
+                var newGuid = Guid.NewGuid();
+                // Generate a Guid under which to store the upload
+                var newProjectDir = Path.Combine(physicalProjectsRootDirectory, newGuid.ToString());
+                Directory.CreateDirectory(newProjectDir);
+                foreach (var fileEntry in projectArchive.Entries)
                 {
-                    using (var currentEntryStream = fileEntry.Open())
+                    var neededPath = new FileInfo(Path.Combine(newProjectDir, fileEntry.FullName)).Directory.FullName;
+                    if (!Directory.Exists(neededPath))
                     {
-                        using (var fileStream = System.IO.File.Create(Path.Combine(neededPath, fileEntry.Name)))
+                        Directory.CreateDirectory(neededPath);
+                    }
+                    // Copy only when it's a file and not a folder
+                    if (fileEntry.Length > 0)
+                    {
+                        using (var currentEntryStream = fileEntry.Open())
                         {
-                            currentEntryStream.CopyTo(fileStream);
+                            using (var fileStream = File.Create(Path.Combine(neededPath, fileEntry.Name)))
+                            {
+                                currentEntryStream.CopyTo(fileStream);
+                            }
                         }
                     }
                 }
+                // Delete previous folder and set guid to new folder
+                var oldGuid = databaseProject.FolderGuid;
+                databaseProject.FolderGuid = newGuid;
+                context.SaveChanges();
+                var oldFolder = Path.Combine(physicalProjectsRootDirectory, oldGuid.ToString());
+                if (Directory.Exists(oldFolder) || oldGuid != Guid.Empty)
+                {
+                    Directory.Delete(oldFolder, true);
+                }
+                return true;
             }
-            // Delete previous folder and set guid to new folder
-            var oldGuid = databaseProject.FolderGuid;
-            databaseProject.FolderGuid = newGuid;
-            context.SaveChanges();
-            var oldFolder = Path.Combine(physicalProjectsRootDirectory, oldGuid.ToString());
-            if (Directory.Exists(oldFolder) || oldGuid != Guid.Empty)
+            catch
             {
-                Directory.Delete(oldFolder, true);
+                // Could not write the files to disk
+                return false;
             }
-            return true;
         }
     }
 }
