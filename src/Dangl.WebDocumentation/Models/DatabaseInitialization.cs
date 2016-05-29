cfbed220f37829c80b2e9ae1c2fc523919f6da98
Georg Dangl <georg@georgdangl.de>
2016-05-29 19:12:16 +0200

    Fix creation of admin role in DB initialization

--- src/Dangl.WebDocumentation/Models/DatabaseInitialization.cs
+++ src/Dangl.WebDocumentation/Models/DatabaseInitialization.cs
@@ -13,9 +13,15 @@ namespace Dangl.WebDocumentation.Models
         private static void SetUpRoles(ApplicationDbContext Context)
         {
             // Add Admin role if not present
-            if (Context.Roles.All(Role => Role.Name != "Admin"))
+            if (Context.Roles.All(role => role.Name != "Admin"))
             {
-                Context.Roles.Add(new IdentityRole {Name = "Admin"});
+                Context.Roles.Add(new IdentityRole {Name = "Admin", NormalizedName = "ADMIN"});
+                Context.SaveChanges();
+            }
+            else if (Context.Roles.Any(role => role.Name == "Admin" && role.NormalizedName != "Admin"))
+            {
+                var adminRole = Context.Roles.FirstOrDefault(role => role.Name == "Admin");
+                adminRole.NormalizedName = "ADMIN";
                 Context.SaveChanges();
             }
         }
