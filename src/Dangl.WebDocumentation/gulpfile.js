ff062d17a5b1307301a3bbdac1d717d1005a009b
Georg Dangl <georg@dangl.me>
2018-02-01 13:00:07 +0100

    Move frontend dependencies from bower to npm and update to latest versions

--- src/Dangl.WebDocumentation/gulpfile.js
+++ src/Dangl.WebDocumentation/gulpfile.js
@@ -17,6 +17,19 @@ paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
+var frontEndPackages = ["jquery", "bootstrap"];
+
+gulp.task("copy:lib",
+    function (cb) {
+        gulp.src([
+                "./node_modules/jquery/**/*"
+            ])
+            .pipe(gulp.dest("./wwwroot/lib/jquery"));
+        return gulp.src([
+                "./node_modules/bootstrap/**/*"
+            ])
+            .pipe(gulp.dest("./wwwroot/lib/bootstrap"));
+    });
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
@@ -26,7 +39,11 @@ gulp.task("clean:css", function (cb) {
     rimraf(paths.concatCssDest, cb);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+gulp.task("clean:lib", function (cb) {
+    rimraf("./wwwroot/lib", cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css", "clean:lib"]);
 
 gulp.task("min:js", function () {
     return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
