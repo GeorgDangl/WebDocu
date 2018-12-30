9ec103329b0d56e0c503ec7cd46a4413fcadcbbf
Georg Dangl <georg@dangl.me>
2018-12-30 17:47:05 +0100

    Add status controller

---
+++ src/Dangl.WebDocumentation/Controllers/API/StatusController.cs
@@ -0,0 +1,32 @@
+﻿using Dangl.WebDocumentation.Services;
+using Dangl.WebDocumentation.ViewModels.Status;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dangl.WebDocumentation.Controllers.API
+{
+    /// <summary>
+    /// This controller reports the health status of the Dangl.Docu API
+    /// </summary>
+    [Route("api/status")]
+    [AllowAnonymous]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        /// <summary>
+        /// Reports the health status of the Dangl.Docu API
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("")]
+        [ProducesResponseType(typeof(StatusGet), 200)]
+        public IActionResult GetStatus()
+        {
+            var status = new StatusGet
+            {
+                IsHealthy = true,
+                Version = VersionsService.Version
+            };
+            return Ok(status);
+        }
+    }
+}
---
+++ src/Dangl.WebDocumentation/ViewModels/Status/StatusGet.cs
@@ -0,0 +1,18 @@
+﻿namespace Dangl.WebDocumentation.ViewModels.Status
+{
+    /// <summary>
+    /// Indicates the status of the Dangl.Docu service
+    /// </summary>
+    public class StatusGet
+    {
+        /// <summary>
+        /// If any problems in the service health are known, this is set to false
+        /// </summary>
+        public bool IsHealthy { get; set; }
+
+        /// <summary>
+        /// The current version of the Dangl.Docu service
+        /// </summary>
+        public string Version { get; set; }
+    }
+}
