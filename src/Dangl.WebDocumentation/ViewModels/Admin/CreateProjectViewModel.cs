5e14f6e5c19d4ea763f13278e7ad93d5c9e7e32c
Georg Dangl <georg@dangl.me>
2019-01-02 21:38:32 +0100

    Mark required properties in ViewModel

--- src/Dangl.WebDocumentation/ViewModels/Admin/CreateProjectViewModel.cs
+++ src/Dangl.WebDocumentation/ViewModels/Admin/CreateProjectViewModel.cs
@@ -6,12 +6,14 @@ namespace Dangl.WebDocumentation.ViewModels.Admin
     public class CreateProjectViewModel
     {
         [Display(Name ="Name")]
+        [Required]
         public string ProjectName { get; set; }
 
         [Display(Name ="Public access")]
         public bool IsPublic { get; set; }
 
         [Display(Name ="Path to index")]
+        [Required]
         public string PathToIndexPage { get; set; }
 
         [Display(Name ="Users with access")]
